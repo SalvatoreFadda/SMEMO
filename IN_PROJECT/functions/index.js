b5d8cd2bf2b6c2b86a3707a4f2b755c91f6354b2
SalvatoreFadda <salvatoref1996@gmail.com>
2019-12-06 21:18:25 +0100

    altro

--- IN_PROJECT/functions/index.js
+++ IN_PROJECT/functions/index.js
@@ -1,4 +1,3 @@
-// const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,6 +5,7 @@
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
 
 const functions = require('firebase-functions');
 const {
@@ -19,13 +19,67 @@ const app = dialogflow({
   debug: true
 });
 
-//intents follow
-
 app.intent('Default Welcome Intent', conv => {
-  conv.ask('Which do you want to show? Robot? Bottone? Or, Impostazioni?');
+  conv.ask('Ciao, bentornato !');
   conv.ask(new HtmlResponse({
     url: `https://${firebaseConfig.projectId}.firebaseapp.com/`
   }));
 });
+
+app.intent('Insegnare(1)', conv => {
+  conv.ask('In quale contesto vuoi insegnarmi ?');
+  const parameters = {
+  };
+  conv.contexts.set('Contesto', 1, parameters);
+  conv.ask(new HtmlResponse({
+    data: {
+      scene: 'insegnami',
+    }
+  }));
+});
+
+app.intent('Contesto(2-new)', conv => {
+  conv.ask(`A quale domanda devo rispondere ?`);
+  var contestoDatoDaUser = conv.parameters.categoria;
+  const parameters = {
+    contestoDaUser: contestoDatoDaUser,
+  };
+  conv.contexts.set('Domanda', 1, parameters);
+});
+
+app.intent('Contesto(2)Fallback', conv => {
+  conv.ask('Scusa ma non conosco questa categoria, scegline un altra');
+});
+
+app.intent('Domanda(3)', conv => {
+  conv.ask('Che risposta devo dare ?');
+  var contestoDatoDaUser = conv.contexts.input.domanda.parameters.contestoDaUser
+  // contestoDatoDaUser = conv.parameters.contestoDaUser;
+  const parameters = {
+     contestoDatoDaUser: contestoDatoDaUser, domandaDatoDaUser: conv.input.raw
+  };
+  conv.contexts.set('Risposta', 1, parameters);
+});
+
+app.intent('Risposta(4)', conv => {
+  conv.ask('Benissimo, ho imparato qualcosa di nuovo !')
+  var contestoDatoDaUser = conv.contexts.input.risposta.parameters.contestoDatoDaUser;
+  var domandaDatoDaUser = conv.contexts.input.risposta.parameters.domandaDatoDaUser;
+  const parameters = {
+     contestoDatoDaUser: contestoDatoDaUser, domandaDatoDaUser: domandaDatoDaUser, risposta: conv.input.raw
+  };
+  conv.contexts.set('CreazioneIntento', 1, parameters);
+
+  conv.ask(new HtmlResponse({
+    data: {
+      scene: 'createIntent',
+    }
+  }));
+
+  //CreateIntent(contestoDatoDaUser, domandaDatoDaUser, conv.input.raw);
+});
+
+
+// TODO: Write your code here.
 
 exports.fulfillment = functions.https.onRequest(app);
