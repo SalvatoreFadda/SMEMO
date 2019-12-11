94f3e02d8e0b1eb2576331edbd5be20554a908fc
SalvatoreFadda <salvatoref1996@gmail.com>
2019-12-11 12:21:12 +0100

    done function

--- IN_PROJECT/public/js/main.js
+++ IN_PROJECT/public/js/main.js
@@ -1,40 +1,42 @@
 'use strict';
 
+
 //interactive canvas call response
 interactiveCanvas.ready({
   onUpdate(data) {
     //visualizzazione pagina dove il bambino insegna a SMEMO
     if (data.scene === 'insegnami') {
-      document.querySelector('#welcome').style.display = 'none';
+      noneSelector('insegnami');
+      document.querySelector('#impostazioni').style.display = 'block';
+      document.querySelector('#tutorial').style.display = 'block';
+      document.querySelector('#cards').style.display = 'block';
+      document.querySelector('#bottone').style.display = 'block';
+      document.querySelector('#robotCentro').style.display = 'block';
       document.querySelector('#backHome').style.display = 'block';
       document.querySelector('#insegnami').style.display = 'block';
     }
     //visualizzazione pagina della richietsa del bambino a SMEMO
     if (data.scene === 'chiedimi') {
-      document.querySelector('#welcome').style.display = 'none';
+      noneSelector('chiedimi');
+      document.querySelector('#impostazioni').style.display = 'block';
+      document.querySelector('#tutorial').style.display = 'block';
+      document.querySelector('#cards').style.display = 'block';
+      document.querySelector('#bottone').style.display = 'block';
       document.querySelector('#backHome').style.display = 'block';
-      document.querySelector('#chiedimiMess').innerText = data.message;
+      document.querySelector('#robotCentro').style.display = 'block';
+      //document.querySelector('#chiedimiMess').innerText = data.message;
       document.querySelector('#chiedimi').style.display = 'block';
-	//document.querySelector('#chiedimiMessage').innerText = data.message;
      }
     //passaggio dalla creazione degli intenti alla visualizazzione degli intenti creati
-    if (data.scene === 'createIntent') {
-      document.querySelector('#robotCentro').style.display = 'none';
-      document.querySelector('#insegnami').style.display = 'none';
-      //scritta con il nome dell'intento
-      //scitta a quale domanda devo rispondere? --> sempre la stessa
+    if (data.scene === 'createIntent') {  //NON PRESENTE NELLA DEMO 
+      noneSelector('createIntent');
       document.querySelector('#robotDx').style.display = 'block';
+      document.querySelector('#bottone').style.display = 'block';
       document.querySelector('#singleCard').style.display = 'block';
     }
     //visualizzazione della card appena creata
     if (data.scene === 'newCard'){
-      document.querySelector('#robotDx').style.display = 'none';
-      document.querySelector('#singleCard').style.display = 'none';
-      document.querySelector('#impostazioni').style.display = 'none';
-      document.querySelector('#tutorial').style.display = 'none';
-      document.querySelector('#impostazioni').style.display = 'none';
-      document.querySelector('#cards').style.display = 'none';
-      document.querySelector('#backHome').style.display = 'none';
+      noneSelector('newCard');
       document.querySelector('#intentVideo').style.display = 'block';
       document.getElementById("tada").autoplay = true;
       document.getElementById("tada").load();
@@ -44,47 +46,91 @@ interactiveCanvas.ready({
         document.querySelector('#bottone').style.display = 'block';
         document.querySelector('#backHome').style.display = 'block';
         document.querySelector('#newCard').style.display = 'block';
-      }, 5000);
+      }, 7000);
     }
-    //visualizzazione impostazioni, di default parte l'impostazione della voce(che si occupa di cambiare il colore al robot)
+    //visualizzazione impostazioni, di default parte l'impostazione della voce(che si occupa di cambiare anche il colore del robot)
     if (data.scene === 'impostazioni'){
-      //alla fine dello sviluppo scrivere tutti gli id delle pagine da cui può partire a none
+      noneSelector('impostazioni');
       document.querySelector('#backHome').style.display = 'block';
       document.querySelector('#tutorial').style.display = 'block';
+      document.querySelector('#bottone').style.display = 'block';
       document.querySelector('#robotDx').style.display = 'block';
       document.querySelector('#voce').style.display = 'block';
       //immagine voce più grnade, altre 2 pù piccole
     }
     //visualizzazione impostazioni per cambio sfondo
-    if (data.scene === 'sfondo'){
-      //alla fine dello sviluppo scrivere tutti gli id delle pagine da cui può partire a none
+    if (data.scene === 'sfondi'){
+      noneSelector('sfondi');
       document.querySelector('#backHome').style.display = 'block';
       document.querySelector('#tutorial').style.display = 'block';
+      document.querySelector('#bottone').style.display = 'block';
       document.querySelector('#robotDx').style.display = 'block';
-      document.querySelector('#sfondo').style.display = 'block';
+      document.querySelector('#sfondi').style.display = 'block';
       //immagine sfondo più grnade, altre 2 pù piccole
+    }
+    //visualizzazione impostazioni per cambio nome
+    if (data.scene === 'nome'){
+      noneSelector('nome');
+      document.querySelector('#backHome').style.display = 'block';
+      document.querySelector('#tutorial').style.display = 'block';
+      document.querySelector('#bottone').style.display = 'block';
+      document.querySelector('#robotDx').style.display = 'block';
+      document.querySelector('#nome').style.display = 'block';
+      //immagine nome più grnade, altre 2 pù piccole
     }
     //ritorno alla home page(welcome)
     if (data.scene === 'home'){
-    //alla fine dello sviluppo scrivere tutti gli id delle pagine da cui può partire a none
+      noneSelector('home');
       document.querySelector('#impostazioni').style.display = 'block';
       document.querySelector('#tutorial').style.display = 'block';
+      document.querySelector('#bottone').style.display = 'block';
       document.querySelector('#cards').style.display = 'block';
       document.querySelector('#welcome').style.display = 'block';
     }
-    //visualizzazione delle cards  ------------> impossibile entro il 13
-    if (data.scene === 'cardsPage'){
-    //alla fine dello sviluppo scrivere tutti gli id delle pagine da cui può partire a none
+    //visualizzazione delle cards
+    if (data.scene === 'cardsPage'){ //NON PRESENTE NELLA DEMO
+      noneSelector('cardsPage');
       document.querySelector('#robotDx').style.display = 'block';
     //capire come far uscire tutte le card del bambino...?
     }
     //default page
     else {
+      noneSelector('default');
+      document.querySelector('#bottone').style.display = 'block';
       document.querySelector('#defaultPage').style.display = 'block';    
     }
 
   }
 });
+
+//funzione che si occupa di nascondere gli elementi non necessari
+function noneSelector(scene){
+    document.querySelector('#defaultPage').style.display = 'none';
+    document.querySelector('#robotDx').style.display = 'none';
+    document.querySelector('#backHome').style.display = 'none';
+    document.querySelector('#impostazioni').style.display = 'none';
+    document.querySelector('#tutorial').style.display = 'none';
+    document.querySelector('#robotCentro').style.display = 'none';
+    document.querySelector('#bottone').style.display = 'none';
+    document.querySelector('#cards').style.display = 'none';
+    document.querySelector('#welcome').style.display = 'none';
+    document.querySelector('#insegnami').style.display = 'none';
+    document.querySelector('#chiedimi').style.display = 'none';
+    document.querySelector('#tutorial').style.display = 'none';
+    document.querySelector('#domanda').style.display = 'none';
+    document.querySelector('#risposta').style.display = 'none';
+    document.querySelector('#continua').style.display = 'none';
+    document.querySelector('#newCard').style.display = 'none';
+    document.querySelector('#voce').style.display = 'none';
+    document.querySelector('#sfondi').style.display = 'none';
+    document.querySelector('#nome').style.display = 'none';
+    //....
+    
+}
+
+
+
+
 
 //on click for interactive conversational images
 document.querySelectorAll('#smemo img').forEach(img => {
